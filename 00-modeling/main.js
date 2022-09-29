3d6e2a06b8444681a219cd316ae31d8ecbcfc0d9
unwrittenmike <unwrittenmike@gmail.com>
2022-09-29 20:02:56 +0000

    progress

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -1,4 +1,4 @@
-import '../style.css';
+import "../style.css";
 
 // Create a state machine transition function either using:
 // - a switch statement (or nested switch statements)
@@ -6,3 +6,44 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+const machine = {
+  initial: "loading",
+  states: {
+    loading: {
+      on: {
+        LOADED: "playing",
+      },
+    },
+    playing: {
+      on: {
+        PAUSE: "paused",
+      },
+    },
+    paused: {
+      on: {
+        PLAY: "playing",
+      },
+    },
+  },
+};
+
+function transition(state, event) {
+  const nextStatus =
+    machine.states[state.status].on?.[event.type] ?? state.status;
+
+  return { value: nextStatus };
+}
+
+const stateMachine = {
+  state: { value: machine.initial },
+  on(event) {
+    console.log(event);
+    this.state = { ...transition(this.state, { type: event }) };
+    console.log(this.state);
+  },
+};
+
+window.machine = machine;
+window.transition = transition;
+window.stateMachine = stateMachine;
